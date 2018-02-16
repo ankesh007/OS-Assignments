850c035451a2b44f1567fa7b4f3bb629d575922d
ankesh007 <ankeshgupta007@gmail.com>
2018-02-16 14:18:26 +0530

    Added modified kernel files for Assignment 1

---
+++ Assignment 1/changed_files.sh
@@ -0,0 +1,9 @@
+folder='xv6-os-assig1.1'
+
+cp $folder/syscall.c syscall.c
+cp $folder/sysproc.c sysproc.c
+cp $folder/syscall.h syscall.h
+cp $folder/user.h user.h
+cp $folder/usys.S usys.S
+cp $folder/Makefile Makefile
+cp $folder/proc.c proc.c
---
+++ Assignment 1/sysproc.c
@@ -0,0 +1,130 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "spinlock.h"
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+int toggle_flag;
+
+int 
+sys_toggle(void)
+{
+  // toggle();
+  // printf(0,"Inside sys_toggle\n" );
+  // cprintf("%p,%d lolololo\n",&toggle_flag,toggle_flag);
+  toggle_flag=1-toggle_flag;
+  return 1;
+}
+
+int 
+sys_add(int a,int b)
+{
+  return (a+b);
+}
+
+struct {
+  struct spinlock lock;
+  struct proc proc[NPROC];
+} ptable;
+
+int
+sys_ps()
+{
+  // cprintf("HI\n");
+  struct proc *p;
+
+  acquire(&ptable.lock);
+
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    if(p->state == RUNNABLE || p->state==RUNNING || p->state==SLEEPING)
+      cprintf("pid:%d name:%s\n",p->pid,p->name);
+
+  release(&ptable.lock);
+  return 1;
+}
