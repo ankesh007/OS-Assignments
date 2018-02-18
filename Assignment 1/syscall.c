4c1565f7997699eec09a441c38120347d62ea37b
ankesh007 <ankeshgupta007@gmail.com>
2018-02-18 20:53:31 +0530

    Finalized Code && Added Report

--- Assignment 1/syscall.c
+++ Assignment 1/syscall.c
@@ -133,8 +133,34 @@ static int (*syscalls[])(void) = {
 [SYS_ps]      sys_ps,
 [SYS_add]     sys_add,
 };
+static char* (syscall_names[]) = {
+[SYS_fork]    "sys_fork",
+[SYS_exit]    "sys_exit",
+[SYS_wait]    "sys_wait",
+[SYS_pipe]    "sys_pipe",
+[SYS_read]    "sys_read",
+[SYS_kill]    "sys_kill",
+[SYS_exec]    "sys_exec",
+[SYS_fstat]   "sys_fstat",
+[SYS_chdir]   "sys_chdir",
+[SYS_dup]     "sys_dup",
+[SYS_getpid]  "sys_getpid",
+[SYS_sbrk]    "sys_sbrk",
+[SYS_sleep]   "sys_sleep",
+[SYS_uptime]  "sys_uptime",
+[SYS_open]    "sys_open",
+[SYS_write]   "sys_write",
+[SYS_mknod]   "sys_mknod",
+[SYS_unlink]  "sys_unlink",
+[SYS_link]    "sys_link",
+[SYS_mkdir]   "sys_mkdir",
+[SYS_close]   "sys_close",
+[SYS_toggle]  "sys_toggle",
+[SYS_ps]      "sys_ps",
+[SYS_add]     "sys_add",
+};
 static int call_count[30];
-static char* syscall_names[]={"","sys_fork","sys_exit","sys_wait","sys_pipe","sys_read","sys_kill","sys_exec","sys_fstat","sys_chdir","sys_dup","sys_getpid","sys_sbrk","sys_sleep","sys_uptime","sys_open","sys_write","sys_mknod","sys_unlink","sys_link","sys_mkdir","sys_close","sys_toggle","sys_ps","sys_add"};
+// static char* syscall_names[]={"","sys_fork","sys_exit","sys_wait","sys_pipe","sys_read","sys_kill","sys_exec","sys_fstat","sys_chdir","sys_dup","sys_getpid","sys_sbrk","sys_sleep","sys_uptime","sys_open","sys_write","sys_mknod","sys_unlink","sys_link","sys_mkdir","sys_close","sys_toggle","sys_ps","sys_add"};
 // extern int toggle_flag;
 int toggle_flag=1;
 void
